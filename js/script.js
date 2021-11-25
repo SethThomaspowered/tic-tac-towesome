92b770ee977019c817bc0993f7d19e542ea66758
Seth Thomas <athomas.seth@gmail.com>
2021-11-24 23:18:12 -0500

    squares change colors when clicked.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("Is it working?")
+const squares = document.querySelectorAll(".square");
+const square1 = document.querySelector(".square");
+// Counter determines which player is up.
+let counter = 1
+for (let i =0;  i <squares.length; i++) {
+    squares[i].addEventListener('click', function(event) {
+        if ((squares[i].style.background ==="red") || (squares[i].style.background ==="green")){
+            alert("This space has already been selected. Choose another space");
+        }else{
+            if (counter%2 === 0){
+                squares[i].style.background ="red";
+            }else{
+                squares[i].style.background ="green";
+            }
+            counter++;
+        }
+    }
+)}
+const reset = () => {
+    for (let i =0; i<squares.length; i++){
+        squares[i].style.background='lemonchiffon';
+    }
+    counter = 1
+} 
